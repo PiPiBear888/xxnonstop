5b10c0844a76f331c099648402001ae896ab0bd7
PiPiBear888 <3014777253@qq.com>
2025-01-04 17:48:43 +0800

    Update 智能九键.cpp

--- 智能九键.cpp
+++ 智能九键.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <regex>
 using namespace std;
-class TrieNode 
+class TrieNode
 {
 public:
     unordered_map<char, TrieNode*> children;
@@ -15,19 +15,19 @@ public:
     TrieNode() : count(0), is_end_of_word(false) {}
 };
 
-class Trie 
+class Trie
 {
 private:
     TrieNode* root;
 
 public:
-    Trie() 
+    Trie()
     {
         root = new TrieNode();
     }
 
     // 插入字符串及输入次数
-    void insert(const string& word, int count) 
+    void insert(const string& word, int count)
     {
         TrieNode* node = root;
         for (char c : word) {
@@ -41,7 +41,7 @@ public:
     }
 
     // 删除目标字符串
-    bool remove(const string& word) 
+    bool remove(const string& word)
     {
         return removeHelper(root, word, 0);
     }
@@ -99,7 +99,7 @@ public:
     // 深度优先搜索
     void dfs(TrieNode* node, const string& prefix, vector<pair<string, int>>& result) {
         if (node->is_end_of_word) {
-            result.push_back({prefix, node->count});
+            result.push_back({ prefix, node->count });
         }
         for (auto& child : node->children) {
             dfs(child.second, prefix + child.first, result);
@@ -114,7 +114,7 @@ public:
 
     void queryRegexHelper(TrieNode* node, const string& current_str, vector<pair<string, int>>& result, const regex& reg) {
         if (node->is_end_of_word && regex_match(current_str, reg)) {
-            result.push_back({current_str, node->count});
+            result.push_back({ current_str, node->count });
         }
         for (auto& child : node->children) {
             queryRegexHelper(child.second, current_str + child.first, result, reg);
@@ -122,72 +122,77 @@ public:
     }
 };
 
-int main() 
+int main()
 {
     Trie trie;
-    while(true)
+    while (true)
     {
         int choice{ 0 };
         cout << "star欢迎使用智能九键输入法!" << endl
-        << "请选择您要进行的操作：" << endl
-        << "1.增加目标字符串及对应的输入次数" << endl
-        << "2.删除目标字符串" << endl
-        << "3.修改目标字符串及对应的输入次数" << endl
-        << "4.查询输入法中满足正则表达式的所有单词" << endl
-        << "5.通过输入字符串查询该字符串在输入法中的输入次数" << endl
-        << "6.通过输入字符串查询以该字符串为前缀的所有字符串及其输入次数" << endl
-        << "7.查询输入法中出现频率最高的字符串" << endl
-        << "8.查询输入法中所有字符串及输入次数" << endl
-        << "9.退出程序" << endl
-        << "皮皮熊邀请你请输入操作前的数字：";
-    cin >> choice;
-    if (choice == 1)
-    {
-        string word;
-        int count;
-        trie.insert (word,count);
-    }
+            << "请选择您要进行的操作：" << endl
+            << "1.增加目标字符串及对应的输入次数" << endl
+            << "2.删除目标字符串" << endl
+            << "3.修改目标字符串及对应的输入次数" << endl
+            << "4.查询输入法中满足正则表达式的所有单词" << endl
+            << "5.通过输入字符串查询该字符串在输入法中的输入次数" << endl
+            << "6.通过输入字符串查询以该字符串为前缀的所有字符串及其输入次数" << endl
+            << "7.查询输入法中出现频率最高的字符串" << endl
+            << "8.查询输入法中所有字符串及输入次数" << endl
+            << "9.退出程序" << endl
+            << "皮皮熊邀请你请输入操作前的数字：";
+        cin >> choice;
+        if (choice == 1)
+        {
+            string word;
+            int count;
+            cout << "请输入单词：";
+            cin >> word ;
+            cout << "请输入该单词出现的次数：";
+            cin >> count;
+            trie.insert(word, count);
+            cout << "输入成功";
+        }
 
-    else if (choice == 2)
-    {
-        string word;
-        trie.remove (word);
-    }
+        else if (choice == 2)
+        {
+            string word;
+            trie.remove(word);
+        }
 
-    else if (choice == 3)
-    {
-        cout << "star还没实现" << endl;
-    }
+        else if (choice == 3)
+        {
+            cout << "star还没实现" << endl;
+        }
 
-    else if (choice == 4)
-    {
-        cout << "star还没实现" << endl;
-    }
+        else if (choice == 4)
+        {
+            cout << "star还没实现" << endl;
+        }
 
-    else if (choice == 5)
-    {
-       cout << "ppx和star还没实现" << endl;
-    }
+        else if (choice == 5)
+        {
+            cout << "ppx和star还没实现" << endl;
+        }
 
-    else if (choice == 6)
-    {
-       cout << "ppx和star还没实现" << endl;
-    }
+        else if (choice == 6)
+        {
+            cout << "ppx和star还没实现" << endl;
+        }
 
-    else if (choice == 7)
-    {
-        cout << "ppx和star还没实现" << endl;
-    }
+        else if (choice == 7)
+        {
+            cout << "ppx和star还没实现" << endl;
+        }
 
-    else if (choice == 8)
-    {
-        cout << "ppx和star还没实现" << endl;
-    }
+        else if (choice == 8)
+        {
+            cout << "ppx和star还没实现" << endl;
+        }
 
-    else
-    {
-        cout << "多给一分求求了" << endl;
-    }
+        else
+        {
+            cout << "多给一分求求了" << endl;
+        }
     }
     return 0;
 }
